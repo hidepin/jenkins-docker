fdf3541a201b04f6a62968ac55da63ac380cc0e2
Hidetoshi Imai <hidepin@gmail.com>
2017-07-08 01:31:36 +0900

    Add create ssh-key.

---
+++ jenkins/files/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -f "${JENKINS_HOME}/.ssh/id_rsa.pub" ]; then
+    echo "generate ssh key file."
+    mkdir -p ${JENKINS_HOME}/.ssh
+    ssh-keygen -N '' -f ${JENKINS_HOME}/.ssh/id_rsa
+fi
+
+echo "*************************************************************"
+echo ${JENKINS_HOME}/.ssh/id_rsa.pub
+cat ${JENKINS_HOME}/.ssh/id_rsa.pub
+echo "*************************************************************"
+
+/usr/local/bin/jenkins.sh
