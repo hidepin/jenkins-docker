b6d56761d36b25f99a4a3acd2f640e6dff4c38d9
Hidetoshi Imai <hidepin@gmail.com>
2017-09-06 07:20:49 +0900

    Add Default proxy setting function.

--- jenkins/files/docker-entrypoint.sh
+++ jenkins/files/docker-entrypoint.sh
@@ -13,4 +13,46 @@ echo ${JENKINS_HOME}/.ssh/id_rsa.pub
 cat ${JENKINS_HOME}/.ssh/id_rsa.pub
 echo "*************************************************************"
 
+if [ ! -f ${JENKINS_HOME}/.m2/settings.xml -a \( x${http_proxy} != x"" -o x${https_proxy} != x"" \) ]; then
+    echo "generate proxy setting file."
+    mkdir -p ${JENKINS_HOME}/.m2
+    cat <<EOF > ${JENKINS_HOME}/.m2/settings.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <proxies>
+EOF
+    if [ x${http_proxy} != x"" ]; then
+        proxy_host=`echo ${http_proxy}|sed 's#http://##' | sed 's/:.*//'`
+        proxy_port=`echo ${http_proxy}|sed 's#.*:##'`
+        cat <<EOF >> ${JENKINS_HOME}/.m2/settings.xml
+   <proxy>
+      <id>http</id>
+      <active>true</active>
+      <protocol>http</protocol>
+      <host>${proxy_host}</host>
+      <port>${proxy_port}</port>
+    </proxy>
+EOF
+    fi
+    if [ x${https_proxy} != x"" ]; then
+        proxy_host=`echo ${https_proxy}|sed 's#http://##' | sed 's/:.*//'`
+        proxy_port=`echo ${https_proxy}|sed 's#.*:##'`
+        cat <<EOF >> ${JENKINS_HOME}/.m2/settings.xml
+   <proxy>
+      <id>https</id>
+      <active>true</active>
+      <protocol>https</protocol>
+      <host>${proxy_host}</host>
+      <port>${proxy_port}</port>
+    </proxy>
+EOF
+    fi
+    cat <<EOF >> ${JENKINS_HOME}/.m2/settings.xml
+  </proxies>
+</settings>
+EOF
+fi
+
 exec /usr/local/bin/jenkins.sh
